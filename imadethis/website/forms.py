67134e4750866ecb20d26cc0dedaefea6b8b60f1
Parminder Singh <lakhbir.ls3@gmail.com>
2021-07-20 08:39:44 +0530

    Revert "Trying to add category in the project but not working correctly right now"
    This reverts commit 916ccd4b39715b3d54d42499885e448a9fd03240.

--- imadethis/website/forms.py
+++ imadethis/website/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from .models import *
 
-categories = Category.objects.all().values_list('name', 'name')
 
 class NewProjectForm(forms.ModelForm):
     class Meta:
@@ -12,18 +11,16 @@ class NewProjectForm(forms.ModelForm):
             "name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Project Name"}),
             "made_by": forms.Select(attrs={"class": "form-control", "placeholder": "Who made it?"}),
             "description": forms.Textarea(attrs={"class": "form-control", "placeholder": "Project Description"}),
-            "category": forms.Select(choices=categories, attrs={"class": "form-control", "placeholder": "Project Category"}),
             "link": forms.TextInput(attrs={"class": "form-control", "placeholder": "URL to the Project"}),
         }
         
 class EditProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ["name", "description", "category", "link"]
+        fields = ["name", "description", "link"]
         
         widgets = {
             "name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Project Name"}),
             "description": forms.Textarea(attrs={"class": "form-control", "placeholder": "Project Description"}),
-            "category": forms.Select(choices=categories, attrs={"class": "form-control", "placeholder": "Project Category"}),
             "link": forms.TextInput(attrs={"class": "form-control", "placeholder": "URL to the Project"}),
         }
